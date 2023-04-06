d84bdf18a780b60f61197efe3cdd8c94512e23e1
mehdiaitouchrif <mehdi.aitouchrif@outlook.com>
2023-04-06 14:34:13 +0000

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
@@ -35,6 +36,14 @@ app.get("/api/config/paypal", (req, res) => {
   res.json(process.env.PAYPAL_CLIENT_ID);
 });
 
+// Load React build
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+  });
+}
 // Error middelware
 app.use(errorHandler);
 
